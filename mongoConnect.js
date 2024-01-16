6ea07e5c7411d905c46adc4dc68db75bcc4c35d7
cal20014 <cal20014@byui.edu>
2024-01-16 15:16:03 -0700

    Remove unused MongoDB connection code

--- mongoConnect.js
+++
@@ -1,6 +0,0 @@
-const { MongoClient } = require("mongodb");
-
-async function main() {
-  const uri = "mongodb+srv://cal20014:.8324tech@cluster0.dpfz65k.mongodb.net/";
-  const client = new MongoClient(uri);
-}
